280f6441516e6c9c95eb3debcf8036a9ebc93cf3
Prateek Narang <prateeknarang111@gmail.com>
2018-06-20 18:28:29 +0530

    stree class 2

---
+++ Comp  - 04/dquery.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+#include<cmath>
+using namespace std;
+#define ll long long
+ll seg[2000000],arr[2000000],pos[2000000];
+vector<pair<ll,ll> > vec[2000000];
+void update(ll st,ll l,ll r,ll in,ll v)
+{
+	if(l==r)
+	{
+		seg[st]+=v;
+		return;
+	}
+	ll mid=(l+r)/2;
+	if(in<=mid)
+	{
+		update(st*2+1,l,mid,in,v);
+	}
+	else
+	{
+		update(st*2+2,mid+1,r,in,v);
+	}
+	seg[st]=seg[st*2+1]+seg[st*2+2];
+}
+ll query(ll st,ll l,ll r,ll ql,ll qr)
+{
+	if(l>qr||r<ql)
+	{
+		return 0;
+	}
+	if(l>=ql&&r<=qr)
+	{
+		return seg[st];
+	}
+	ll mid=(l+r)/2;
+	return query(st*2+1,l,mid,ql,qr)+query(st*2+2,mid+1,r,ql,qr);
+}
+int main()
+{
+	ll n;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	ll q;
+	cin>>q;
+	for(int i=0;i<q;i++)
+	{
+		ll l,r;
+		cin>>l>>r;
+		vec[r].push_back(make_pair(l,i));
+	}
+	memset(pos,-1,sizeof(pos));
+	for(int i=0;i<n;i++)
+	{
+		if(pos[arr[i]]!=-1)
+		{
+			update(0,0,n-1,pos[arr[i]],-1);
+		}
+		update(0,0,n-1,i,1);
+		pos[arr[i]]=i;
+		for(int j=0;j<vec[i].size();j++)
+		{
+			ans[vec[i][j].second]=query(0,0,n-1,vec[i][j].first,i);
+		}
+	}
+	for(int i=0;i<q;i++)
+		cout<<ans[i]<<"\n";
+
+	return 0;
+}
---
+++ Comp  - 04/notes.md
@@ -0,0 +1,14 @@
+- Lazy Propagation
+- Matrix Queries-I (HB)
+- Counting Zeroes(COUNZ)
+- HORRIBLE(Spoj)
+- BRCKTS
+- GSS4(Spoj) - Square Root Query
+- MOD(M) Query - 250D Child and Sequence
+	a%m
+	a = km  + r  where r < m
+		r = 2r/2 = <=(m+r)/2 <= (km+r)/2 <= a/2
+
+- DQUERY
+- Spoj
+
